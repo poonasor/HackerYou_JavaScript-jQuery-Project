e678bd6ce16c8324b977e3f9a6292ac381e76269
Ricky Poon <rickypoon89@icloud.com>
2019-03-09 16:08:10 -0500

    namespace update

--- js/functions.js
+++ js/functions.js
@@ -1,93 +1,77 @@
 $(function(){
 
-//Cache variables
-const $userName = $('#userName');
-const $submit = $('#submitUsername');
-const $scoreboard = $('#score');
-const gameDate = new Date();
+const app = {};
+
+app.username = $('#userName');
+app.$submit = $('#submitUsername');
+app.score = $('#score');
+app.gamedate = new Date();
+app.coonButton = $('.selector.coon');
+app.catButton = $('.selector.cat');
+app.bearButton = $('.selector.bear');
+app.flashcard = $('#flashcard');
+app.buttonClick = $('.selector');
 
 let score = 0;
+let count = 1;
 
-$submit.on('click', function() {
-    if( $userName.val() != '' ) {
-//        console.log($userName.val());
+const leaderboard = {
+
+}
+
+app.submit = function() {
+    if( app.username.val() != '' ) {
         $('#info').css('display', 'none');
     } else {
         alert('Please fill in your name');
     }
-});
+}
 
 function addScore(){
     score = score + 1;
 }
 
-//Record when each button is pressed
-const $coonButton = $('.selector.coon');
-$coonButton.on('click', function() {
-//    console.log('Coon', 'Pressed +1');
-
-if ($flashcard.hasClass('slide-01') || $flashcard.hasClass('slide-03') || $flashcard.hasClass('slide-08') || $flashcard.hasClass('slide-10') || $flashcard.hasClass('slide-13') ) {
-    addScore()
-//    console.log('Score:', score);
-    $scoreboard.text(score);
-} else {
-//    console.log('Score:', score);
+app.coonButtonClicked = function() {
+    if (app.flashcard.hasClass('slide-01') || app.flashcard.hasClass('slide-03') || app.flashcard.hasClass('slide-08') || app.flashcard.hasClass('slide-10') || app.flashcard.hasClass('slide-13') ) {
+        addScore()
+        app.score.text(score);
+    }
 }
 
-});
-
-const $catButton = $('.selector.cat');
-$catButton.on('click', function() {
-//    console.log('Cat', 'Pressed +1');
-
-if ($flashcard.hasClass('slide-02') || $flashcard.hasClass('slide-04') || $flashcard.hasClass('slide-05') || $flashcard.hasClass('slide-07') || $flashcard.hasClass('slide-12') ) {
-    addScore()
-//    console.log('Score:', score);
-    $scoreboard.text(score);
-} else {
-//    console.log('Score:', score);
+app.catButtonClicked = function() {
+    if (app.flashcard.hasClass('slide-02') || app.flashcard.hasClass('slide-04') || app.flashcard.hasClass('slide-05') || app.flashcard.hasClass('slide-07') || app.flashcard.hasClass('slide-12') ) {
+        addScore()
+        app.score.text(score);
+    }
 }
 
-});
-
-const $bearButton = $('.selector.bear');
-$bearButton.on('click', function() {
-//    console.log('Bear', 'Pressed +1');
-
-if ($flashcard.hasClass('slide-06') || $flashcard.hasClass('slide-09') || $flashcard.hasClass('slide-11') || $flashcard.hasClass('slide-14') || $flashcard.hasClass('slide-15') || $flashcard.hasClass('slide-16') ) {
-    addScore()
-//    console.log('Score:', score);
-    $scoreboard.text(score);
-} else {
-//    console.log('Score:', score);
+app.bearButtonClicked = function() {
+    if (app.flashcard.hasClass('slide-06') || app.flashcard.hasClass('slide-09') || app.flashcard.hasClass('slide-11') || app.flashcard.hasClass('slide-14') || app.flashcard.hasClass('slide-15') || app.flashcard.hasClass('slide-16') ) {
+        addScore()
+        app.score.text(score);
+    }
 }
 
-});
-
-//Update Flashcard when Selector Button is pressed
-const $flashcard = $('#flashcard');
-const $buttonClick = $('.selector');
-let count = 1;
-$buttonClick.on('click', function(){
+app.buttonClicked = function() {
     count = count + 1;
     if (count <= 9) {
         let newcount = "" + 0 + count;
-        $flashcard.css('background-image', 'url(images/' + newcount + '.jpg)');
-        $flashcard.removeClass();
-        $flashcard.addClass('slide-' + newcount);
+        app.flashcard.css('background-image', 'url(images/' + newcount + '.jpg)');
+        app.flashcard.removeClass();
+        app.flashcard.addClass('slide-' + newcount);
     } else if (count <= 16) {
-        $flashcard.css('background-image', 'url(images/' + count + '.jpg)');
-        $flashcard.css('class', 'slide-' + count );
-        $flashcard.removeClass();
-        $flashcard.addClass('slide-' + count);
+        app.flashcard.css('background-image', 'url(images/' + count + '.jpg)');
+        app.flashcard.css('class', 'slide-' + count );
+        app.flashcard.removeClass();
+        app.flashcard.addClass('slide-' + count);
     } else {
-        $flashcard.css('display', 'none');
+        app.flashcard.css('display', 'none');
         $('#answer-key').css('display', 'none');
-
-        $('.userResult').html(`Hello <span>${$userName.val()}</span> your final score is <span>${score}</span>`);
-        
+        $('.userResult').html(`Hello <span>${app.username.val()}</span> your final score is <span>${app.score.text()}</span>`);
+        console.log(app.gamedate, app.username.val(), app.score.text());
     };
-});
+}
+
 
 
 
@@ -97,4 +81,12 @@ $('#restart').on('click', function(){
     location.reload();
 });
 
+app.init = function(){
+    app.$submit.on('click', app.submit);
+    app.coonButton.on('click', app.coonButtonClicked);
+    app.catButton.on('click', app.catButtonClicked);
+    app.bearButton.on('click', app.bearButtonClicked);
+    app.buttonClick.on('click', app.buttonClicked);
+}
+app.init();
 });
